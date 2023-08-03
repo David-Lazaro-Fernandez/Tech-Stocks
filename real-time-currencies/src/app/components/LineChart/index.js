c62594a55294e42e57f72a793e980c5774798e31
David Lázaro Fernández <57787993+David-Lazaro-Fernandez@users.noreply.github.com>
2023-08-03 03:59:45 +0000

    ERROR WITH THE FULLFILLMENT

--- real-time-currencies/src/app/components/LineChart/index.js
+++ real-time-currencies/src/app/components/LineChart/index.js
@@ -1,6 +1,10 @@
+import { useState } from "react";
 import Chart from "react-apexcharts";
-import {options} from './options';
 export const LineChart = (props) =>{
+    const {options} = props;
+    
+
+
     return (
         <>
             <Chart 
--- real-time-currencies/src/app/page.js
+++ real-time-currencies/src/app/page.js
@@ -7,22 +7,53 @@ import LineChart from './components/LineChart';
 
 export default function Home() {
   const [stockInfo, setStockInfo] = useState([])
-  
+
+  const [options, setOptions] = useState({
+    options: {
+      chart: {
+        id: "basic-bar"
+      },
+      xaxis: {
+        categories: []
+      }
+    },
+    series: [
+      {
+        name: "series-1",
+        data: [30, 40, 45, 50, 49, 60, 70, 91,12,11]
+      }
+    ]
+  })
+
+
   useEffect(() => {
     async function getInfo() {
       try {
         const APIKEY = "TBxDsx1YUlUNmatc_OQhkvuyOxYXYydR"
         const data = await fetch(`https://api.polygon.io/v2/aggs/ticker/AAPL/range/1/minute/2023-08-01/2023-08-02?adjusted=true&sort=asc&limit=120&apiKey=${APIKEY}`)
-        const jsonData = await data.json() 
+        const jsonData = await data.json()
         setStockInfo(jsonData.results)
       } catch (error) {
         console.error(error)
       }
     }
+
     getInfo()
   }, [])
 
   useEffect(() => {
+    const fillChart = (number) => {
+      for (let i = 0; i < number; i++) {
+        stockInfo[i].t ? console.log(stockInfo[i]) : null
+        //options.options.xaxis.categories.append(setCategorie(stockInfo[i].t))
+      }
+    }
+    const setCategorie = (timestamp) => {
+      date = new Date(timestamp)
+      return date
+    }
+
+    fillChart(10)
     console.log("Hey now I'm in a state", stockInfo)
   }, [stockInfo])
 
@@ -31,8 +62,8 @@ export default function Home() {
   //
   return (
     <main className={styles.main}>
-      <LineChart />
-        
+      <LineChart stockInfo={stockInfo} options={options}/>
+
 
     </main>
   )
